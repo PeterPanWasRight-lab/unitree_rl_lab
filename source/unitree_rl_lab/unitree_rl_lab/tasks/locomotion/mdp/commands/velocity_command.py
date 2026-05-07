9dbf3b6cbd06ccd18dd7e70f6dccceac2e1de5d1
PeterPanWasRight-lab <107916107+PeterPanWasRight-lab@users.noreply.github.com>
2026-05-07 22:07:54 +0800

    一些注释

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/mdp/commands/velocity_command.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/mdp/commands/velocity_command.py
@@ -5,7 +5,10 @@ from dataclasses import MISSING
 from isaaclab.envs.mdp import UniformVelocityCommandCfg
 from isaaclab.utils import configclass
 
-
+# C:\Users\17547\miniconda3\envs\IsaacLab51\Lib\site-packages\isaaclab\source\isaaclab\isaaclab\envs\mdp\commands\velocity_command.py 可以看到具体定义
 @configclass
 class UniformLevelVelocityCommandCfg(UniformVelocityCommandCfg):
-    limit_ranges: UniformVelocityCommandCfg.Ranges = MISSING
+    # 父类UniformVelocityCommandCfg的Ranges已经定义了lin_vel_x, lin_vel_y, ang_vel_z的范围,
+    # 且父类中有一个ranges实例，用于记录当前范。
+    # 这里我们添加一个和Range结构一样的新的属性来限制这个范围的最大值
+    limit_ranges: UniformVelocityCommandCfg.Ranges = MISSING   # MISSING表示创建时必须手动提供，否则报错
